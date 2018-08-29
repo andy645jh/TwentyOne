a9140197d2c7685cc289a0bba4c6d5b1430f50f8
Andy Jhonson <andy645jh@gmail.com>
2018-08-28 21:50:59 -0500

    Se agrega seguimiento de la camara

---
+++ Assets/FollowDisc.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowDisc : MonoBehaviour {
+
+	public Disc disc;
+	private float _max = -10;
+	private float _min = -18;
+
+	void Start () {
+		
+	}
+		
+	void Update () {	
+		var posZ = disc.transform.position.z;
+		posZ = Mathf.Clamp(posZ,_min, _max);
+		Debug.Log("PosZ: "+posZ);
+		transform.position = new Vector3(transform.position.x, transform.position.y, posZ);
+	}
+}
